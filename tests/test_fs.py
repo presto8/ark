28f777c1edf1cb56d1e228e47b2e8936d2cd95b6
Preston Hunt <me@prestonhunt.com>
2022-06-16 08:10:30 -0700

    add basic filesystem test

---
+++ tests/test_fs.py
@@ -0,0 +1,22 @@
+import os
+import stat
+
+from types import SimpleNamespace
+
+from helpers import create_file
+
+
+def create_test_files(tmp_path):
+    foo = tmp_path / "foo"
+    create_file(foo / "hello.txt", "hello, world\n")
+    bar = foo / "bar"
+    create_file(bar / "hola.txt", "hola, mundo\n")
+    zzz = foo / "zzz"
+    os.symlink("bar", zzz)
+
+    return SimpleNamespace(foo=foo, bar=bar, zzz=zzz)
+
+
+def test_regular_file(tmp_path):
+    files = create_test_files(tmp_path)
+    print(files.foo.name)
