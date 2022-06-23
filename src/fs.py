65d1e31409d5e2d68e6771b4e9eaf75936becae8
Preston Hunt <me@prestonhunt.com>
2022-06-22 17:34:30 -0700

    basic filesystem functionality and hashing

--- src/fs.py
+++ src/fs.py
@@ -23,45 +23,42 @@ class ArkObject:
 
 
 @dataclass
-class FsChild:
+class FsEntry:
     path: Path
-    ctime_ns: Optional[int] = None  # None for a parent
-    b2: Optional[bytes] = None      # None for a parent
 
-    @property
-    def is_parent(self):
-        return self.b2 is None and self.ctime_ns is None
+
+@dataclass
+class FsChild(FsEntry):
+    ctime_ns: Optional[int] = None
+    b2: Optional[bytes] = None
 
     def update(self, ctime_ns=True, b2=True):
-        if self.is_parent:
-            return
         if ctime_ns:
             self.ctime_ns = self.path.stat(follow_symlinks=False).st_ctime_ns
         if b2:
             if self.path.is_symlink():
-                contents = os.readlink(self.path.path).encode()
+                contents = str(self.path.readlink()).encode()
             else:
                 contents = open(self.path, "rb").read()
             self.b2 = crypto.blake2b(contents)
 
     @property
     def _msgpack(self):
-        payload = [self.path.name, self.ctime_ns, self.b2]
         return msgpack.packb([self.path.name, self.ctime_ns, self.b2])
 
     @property
     def tpch(self):
-        if not self.ctime_ns or not self.b2:
+        if not self.b2:
             return None
         return crypto.blake2b(self._msgpack)
 
 
 @dataclass
-class FsParent:
+class FsParent(FsEntry):
     hostname: str
     fsid: str
-    path: Path
-    children: list[FsChild] = field(default_factory=list)
+    children: list[FsEntry] = field(default_factory=list)
+    loaded: bool = False
 
     @property
     def ctime_ns(self) -> Optional[int]:
@@ -73,7 +70,9 @@ class FsParent:
     @property
     def tpch(self) -> Optional[bytes]:
         try:
-            payload = b"".join(x.tpch for x in self.children)
+            child_tpch = [x.tpch for x in self.children]
+            child_tpch.sort()
+            payload = msgpack.packb(child_tpch)
             return crypto.blake2b(payload)
         except TypeError:
             return None
@@ -83,13 +82,28 @@ class FsParent:
             child.update()
 
 
-def get_parent(path) -> FsParent:
-    base = dict(hostname=platform.node(), fsid='TODO', path=Path(path))
+def get_parent(path, max_depth=-1) -> FsParent:
+    print('parent')
+    hostname = platform.node()
+    fsid = 'TODO'
     children = []
+    next_child: FsEntry
     for entry in os.scandir(path):
-        next_child = dict(path=entry)
-        if not entry.is_dir(follow_symlinks=False):
-            next_child['ctime_ns'] = entry.stat(follow_symlinks=False).st_ctime_ns
-        children.append(FsChild(**next_child))
+        print(entry)
+        if entry.is_dir(follow_symlinks=False):
+            print(max_depth)
+            if max_depth == 0:  # stop recursion
+                next_child = FsParent(hostname=hostname, fsid=fsid, path=Path(entry))
+            else:
+                print(max_depth)
+                next_child = get_parent(entry, max_depth=max_depth - 1)
+        else:
+            next_child = FsChild(path=Path(entry))
+            next_child.update(b2=False)
+        children.append(next_child)
 
-    return FsParent(**base, children=sorted(children, key=lambda x: x.path.name))
+    children.sort(key=lambda x: x.path.name)
+    for child in children:
+        print(child)
+
+    return FsParent(hostname=hostname, fsid=fsid, path=Path(path), children=children, loaded=True)
--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,21 +1,122 @@
 import os
-
+import msgpack
 from types import SimpleNamespace
-
+from src import crypto
+from src import fs
 from helpers import create_file
 
 
-def create_test_files(tmp_path):
-    foo = tmp_path / "foo"
-    create_file(foo / "hello.txt", "hello, world\n")
-    bar = foo / "bar"
-    create_file(bar / "hola.txt", "hola, mundo\n")
-    zzz = foo / "zzz"
-    os.symlink("bar", zzz)
+def create_test_files(dir_path, structure):
+    for name, value in structure.items():
+        if isinstance(value, str):
+            if value.startswith("link:"):
+                # create a symlink
+                os.symlink(value[5:], dir_path / name)
+            else:
+                # create a file
+                create_file(dir_path / name, value)
+        elif isinstance(value, dict):
+            # directory - recurse
+            create_test_files(dir_path / name, value)
+        else:
+            # error
+            raise
 
-    return SimpleNamespace(foo=foo, bar=bar, zzz=zzz)
+
+def test_simple_files(tmp_path):
+    # directory containing only files (depth=0)
+    create_test_files(tmp_path, {"world.txt": "world\n", "hello.txt": "hello\n"})
+    parent = fs.get_parent(tmp_path)
+
+    assert len(parent.children) == 2
+    # children must be sorted alphabetically by name
+    assert parent.children[0].path.name == "hello.txt"
+    assert parent.children[1].path.name == "world.txt"
+    assert parent.tpch is None
+    # ctime_ns of parent is max(ctime_ns) of children
+    assert parent.ctime_ns == (tmp_path / "world.txt").stat().st_ctime_ns
 
 
-def test_regular_file(tmp_path):
-    files = create_test_files(tmp_path)
-    print(files.foo.name)
+def test_symlink(tmp_path):
+    # add a symlink
+    create_test_files(tmp_path, {"hello.txt": "hello\n", "world.txt": "world\n", "default.txt": "link:world.txt"})
+    parent = fs.get_parent(tmp_path)
+
+    assert len(parent.children) == 3
+    assert parent.tpch is None
+    # ctime_ns of parent is max(ctime_ns) of children
+    assert parent.ctime_ns == os.lstat(tmp_path / "default.txt").st_ctime_ns
+
+
+def test_b2(tmp_path):
+    create_test_files(tmp_path, {"hello.txt": "hello\n", "world.txt": "world\n", "default.txt": "link:world.txt"})
+    parent = fs.get_parent(tmp_path)
+    parent.update()
+
+    assert parent.children[0].path.name == "default.txt"
+
+    world_b2 = crypto.blake2b(open(tmp_path / "world.txt", "rb").read())
+    assert parent.children[-1].b2 == world_b2
+
+
+def test_msgpack_file(tmp_path):
+    create_test_files(tmp_path, {"hello.txt": "hello\n", "world.txt": "world\n", "default.txt": "link:world.txt"})
+    parent = fs.get_parent(tmp_path)
+    parent.update()
+
+    # msgpack and tpch for child 1
+    c0 = parent.children[1]
+    assert c0.path.name == "hello.txt"
+    c0.ctime_ns = 100
+    c0_b2 = crypto.blake2b(b"hello\n")
+    assert c0.b2 == c0_b2
+    payload = ["hello.txt", 100, c0_b2]
+    expected = msgpack.packb(payload)
+    assert c0._msgpack == expected
+    assert c0.tpch == crypto.blake2b(expected)
+
+
+def test_tpch_simple_dir(tmp_path):
+    create_test_files(tmp_path, {"hello.txt": "hello\n", "world.txt": "world\n"})
+    parent = fs.get_parent(tmp_path)
+    parent.update()
+
+    # tpch for child 0
+    c0 = parent.children[0]
+    c0.ctime_ns = 100
+    c0_b2 = crypto.blake2b(b"hello\n")
+    c0_tpch = crypto.blake2b(msgpack.packb(["hello.txt", 100, c0_b2]))
+    assert c0.tpch == c0_tpch
+
+    # tpch for child 1
+    c1 = parent.children[1]
+    c1.ctime_ns = 200
+    c1_b2 = crypto.blake2b(b"world\n")
+    c1_tpch = crypto.blake2b(msgpack.packb(["world.txt", 200, c1_b2]))
+    assert c1.tpch == c1_tpch
+
+    joined = [c0_tpch, c1_tpch]
+    joined.sort()
+
+    parent_tpch = crypto.blake2b(msgpack.packb(joined))
+    assert parent_tpch == parent.tpch
+
+
+def test_tpch_timestamp_change(tmp_path):
+    create_test_files(tmp_path, {"hello.txt": "hello\n", "world.txt": "world\n"})
+    parent = fs.get_parent(tmp_path)
+    parent.update()
+
+    tpch1 = parent.tpch
+    assert parent.tpch is not None
+
+    parent.children[1].ctime_ns = 200
+    assert parent.tpch != tpch1
+
+
+def test_tpch_deep_dir(tmp_path):
+    create_test_files(tmp_path, {"hello.txt": "hello\n", "subdir": {"world.txt": "world\n"}})
+    parent = fs.get_parent(tmp_path)
+    parent.update()
+
+    assert len(parent.children) == 2
