474b8ab17084fa31c002375c880fc4bfc349686a
Preston Hunt <me@prestonhunt.com>
2022-06-23 20:36:08 -0700

    store: minimum functionality

--- src/store.py
+++ src/store.py
@@ -1,10 +1,22 @@
+import os
 from dataclasses import dataclass
-from typing import Optional
+from pathlib import Path
 
 
 @dataclass
 class Store:
-    pass
+    path: Path
+
+    def get(self, name):
+        return open(self.path / name, "rb").read()
+
+    def put(self, name, value):
+        with open(self.path / name, "wb") as f:
+            f.write(value)
+
+    def have(self, name):
+        return os.path.exists(self.path / name)
+
     # guid: str
     # label: Optional[str] = None
     # cache
