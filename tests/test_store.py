4a0127af50ad14216514adffb70e8ebeae61927d
Preston Hunt <me@prestonhunt.com>
2022-06-23 20:36:08 -0700

    store: minimum functionality

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,6 +1,11 @@
 from src import store
 
 
-def test_store_init():
-    s = store.Store()
-    assert s
+def test_store_basic(tmp_path):
+    s = store.Store(tmp_path)
+
+    assert not s.have("blah")
+
+    s.put("hello.txt", b"hello\n")
+    assert s.have("hello.txt")
+    assert s.get("hello.txt") == b"hello\n"
