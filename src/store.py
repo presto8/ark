0033375bc3693488cd0836c9f5da640950d4ba47
Preston Hunt <me@prestonhunt.com>
2022-06-25 08:29:24 -0700

    store: development, still needs work

--- src/store.py
+++ src/store.py
@@ -1,21 +1,59 @@
+import base64
 import os
 from dataclasses import dataclass
 from pathlib import Path
+from src import fs
+
+
+def b64e(data):
+    if isinstance(data, str):
+        data = data.encode()
+    return base64.b64encode(data, altchars=b"+-").decode()
+
+
+def b64d(data):
+    return base64.b64decode(data, altchars=b"+-").decode()
+
+
+def parse_object(obj) -> tuple[str, bytes]:
+    if isinstance(obj, fs.FsChild):
+        return f"{b64e(obj.pth)}_{b64e(obj.ptch)}", b""
+    elif isinstance(obj, str):
+        return obj, b""
+    else:
+        raise
 
 
 @dataclass
 class Store:
     path: Path
 
-    def get(self, name):
+    def __post_init__(self):
+        print(type(self.path))
+        os.makedirs(self.path)
+
+    def put(self, obj) -> str:
+        "Returns name of path on store"
+        if isinstance(obj, fs.FsChild):
+            name = f"{b64e(obj.pth)}_{b64e(obj.ptch)}"
+            data = b""
+        else:
+            raise
+        self.putb(name, data)
+        return name
+
+    def getb(self, name: str) -> bytes:
         return open(self.path / name, "rb").read()
+        return open(self.path / b64e(name), "rb").read()
 
-    def put(self, name, value):
+    def putb(self, name: str, data: bytes):
         with open(self.path / name, "wb") as f:
-            f.write(value)
+            f.write(data)
 
-    def have(self, name):
+    def have(self, obj) -> bool:
+        name, _ = parse_object(obj)
         return os.path.exists(self.path / name)
+
 
     # guid: str
     # label: Optional[str] = None
--- tests/test_store.py
+++ tests/test_store.py
@@ -2,10 +2,10 @@ from src import store
 
 
 def test_store_basic(tmp_path):
-    s = store.Store(tmp_path)
+    s = store.Store(tmp_path / "store")
 
     assert not s.have("blah")
 
-    s.put("hello.txt", b"hello\n")
+    s.putb("hello.txt", b"hello\n")
     assert s.have("hello.txt")
-    assert s.get("hello.txt") == b"hello\n"
+    assert s.getb("hello.txt") == b"hello\n"
