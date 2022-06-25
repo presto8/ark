21545089c2ccd1d80f52c629ede8010ba94196a0
Preston Hunt <me@prestonhunt.com>
2022-06-25 16:10:59 -0700

    test/store: add base64 test vectors from RFC 4648

--- tests/test_store.py
+++ tests/test_store.py
@@ -9,3 +9,21 @@ def test_store_basic(tmp_path):
     s.putb("hello.txt", b"hello\n")
     assert s.have("hello.txt")
     assert s.getb("hello.txt") == b"hello\n"
+
+
+def test_base64():
+    assert store.b64e("") == ""
+    assert store.b64e("f") == "Zg=="
+    assert store.b64e("fo") == "Zm8="
+    assert store.b64e("foo") == "Zm9v"
+    assert store.b64e("foob") == "Zm9vYg=="
+    assert store.b64e("fooba") == "Zm9vYmE="
+    assert store.b64e("foobar") == "Zm9vYmFy"
+
+    assert store.b64d("") == ""
+    assert store.b64d("Zg==") == "f"
+    assert store.b64d("Zm8=") == "fo"
+    assert store.b64d("Zm9v") == "foo"
+    assert store.b64d("Zm9vYg==") == "foob"
+    assert store.b64d("Zm9vYmE=") == "fooba"
+    assert store.b64d("Zm9vYmFy") == "foobar"
